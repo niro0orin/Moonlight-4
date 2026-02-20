7390a8018843d7699197acd437a26574d282f2aa
niro0orin <niro0orin@gmail.com>
2026-02-20 10:34:27 +0200

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Applemed",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
